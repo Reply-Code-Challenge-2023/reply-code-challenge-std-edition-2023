ebf46c44543c179d8d5b878112b810e52f3383e5
Ronan Ragavoodoo <porkypus@gmail.com>
2023-03-09 16:23:57 +0000

    added logic

--- porky.py
+++ porky.py
@@ -7,24 +7,42 @@ matrix = []
 with open("tests/00-example.txt") as topo_file:
     count = 0
     for line in topo_file:
-        vars = line[:-1].split(" ")
+        variables = line[:-1].split(" ")
         if count == 0:
-            columns = vars[0]
-            rows = vars[1]
-            num_snakes = vars[2]
+            columns = variables[0]
+            rows = variables[1]
+            num_snakes = variables[2]
         elif count == 1:
             for n in range(int(num_snakes)):
-                snakes.append(vars[n])
+                snakes.append(variables[n])
         else:
             row = []
             for n in range(int(columns)):
-                row.append(vars[n])
+                row.append(variables[n])
             matrix.append(row)
 
         count += 1
-        print(line[:-1])
 
 max_snake = max(snakes)
+dps = []
+for bound in range(1, len(max_snake) + 1):
+    if bound == 1:
+        dp = list(
+            map(lambda x: list(map(lambda y: 0 if y == "*" else int(y), x)), matrix)
+        )
+    else:
+        dp = [[0] * columns for _ in rows]
+        for row in range(len(matrix)):
+            for col in range(len(matrix[row])):
+                optimal_path = max(
+                    dps[bound - 1][row - 1][col],
+                    dps[bound - 1][row + 1][col],
+                    dps[bound - 1][row][col - 1],
+                    dps[bound - 1][row][col + 1],
+                )
+
+    dps.append(dp)
 
 print(snakes)
 print(matrix)
+print(dps[0])
