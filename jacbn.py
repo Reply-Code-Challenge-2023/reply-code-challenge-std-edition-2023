3ebad22e2055ea4b85d9d435422d84c2c560a330
jacbn <jabrown405@gmail.com>
2023-03-09 17:54:21 +0000

    string direction matrix

--- jacbn.py
+++ jacbn.py
@@ -31,44 +31,45 @@ print(wormholes)
 
 max_snake = max(snakes)
 dps = []
+dps_path = []
 for bound in range(1, max_snake + 1):
     if bound == 1:
         dp = list(map(lambda x: list(map(lambda y: 0 if y == "*" else int(y), x)), matrix))
+        dp_path = [['' for _ in range(cols)] for _ in range(rows)]
     else:
         dp = [[0 for _ in range(cols)] for _ in range(rows)]
+        dp_path = [['' for _ in range(cols)] for _ in range(rows)]
         for row in range(len(matrix)):
             for col in range(len(matrix[row])):
-                
-                # for wormhole in wormholes:
-                #     dist_to_wormhole = abs(wormhole[0] - col) + abs(wormhole[1] - row)
-                #     if dist_to_wormhole <= bound:
                                        
                 adjacent_wormholes = []
                 possible_next = []
+                
                 # for each adjacent cell, check if it's a wormhole
-                for cell in [(col, (row-1)%rows), (col, (row+1)%rows), ((col-1)%cols, row), ((col+1)%cols, row)]:
-                    if cell in wormholes:
+                for cell in [(col, (row-1)%rows, 'U '), (col, (row+1)%rows, 'D '), ((col-1)%cols, row, 'L '), ((col+1)%cols, row, 'R ')]:
+                    if (cell[0], cell[1]) in wormholes:
                         adjacent_wormholes.append(cell)
-                    else:
-                        possible_next.append(cell)
+                    # we can always go up/down/left/right
+                    possible_next.append(cell)
                         
-                for wormhole in adjacent_wormholes:
-                    possible_next += wormholes
+                for adj in adjacent_wormholes:
+                    for i, w in enumerate(wormholes):
+                        possible_next.append((w[0], w[1], f"{adj[2]}{i} "))
                     
-                # remove duplicates
+                # remove duplicates (can there actually be any??)
                 possible_next = list(set(possible_next))
-  
                 prev = dps[bound - 2]
                 
-                choices = [prev[y % rows][x % cols] for x, y in possible_next]
+                choices = [(prev[y % rows][x % cols], direction) for x, y, direction in possible_next]
+                optimal_path, direction = max(choices, key=lambda x: x[0])
                 
-                optimal_path = max(choices)
-                
-                # optimal_path = max(prev[(row - 1) % rows][col], 
-                #                    prev[(row + 1) % rows][col], 
-                #                    prev[row][(col - 1) % cols], 
-                #                    prev[row][(col + 1) % cols])
                 dp[row][col] = optimal_path + (0 if matrix[row][col] == '*' else int(matrix[row][col]))
+                dp_path[row][col] = direction
     dps.append(dp)
+    dps_path.append(dp_path)
 
-print(dps[1])
+for r in dps[1]:
+    print(r)
+
+for l in dps_path[1]:
+    print(l)
