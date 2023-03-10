12269cb44c2b747395cc47f5cf27e62731a82ac5
jacbn <jabrown405@gmail.com>
2023-03-10 13:47:04 +0000

    fix me

--- jacbn.py
+++ jacbn.py
@@ -5,6 +5,49 @@ num_snakes = 0
 matrix = []
 wormholes = []
 
+
+def checkForOverlap(possible_path_index, dps_path, row, col, rows, cols, paths, wormholes):
+    i = len(dps_path)
+    visited = []
+    r = row
+    c = col
+    valid = False
+    
+    print(possible_path_index, len(paths), len(direction))
+    print(paths[possible_path_index], direction[possible_path_index])
+    
+    while (c, r) not in visited:
+        if i > 1:
+            visited.append((c, r))
+            m = dps_path[i-1]
+            # print(r, c)
+            # print(len(m), len(m[0]))
+            direction = m[r][c]
+    
+            if len(direction[possible_path_index]) > 2:
+                index = int(direction[possible_path_index][1:-1])
+                c, r = wormholes[index]
+            else:
+                if direction[0][0] == 'U':
+                    c = (c - 1) % cols
+                elif direction[0][0] == 'D':
+                    c = (c + 1) % cols
+                elif direction[0][0] == 'L':
+                    r = (r - 1) % rows
+                elif direction[0][0] == 'R':
+                    r = (r + 1) % rows
+                else:
+                    raise Exception(f"Unknown direction: {direction[0]}")
+        i -= 1
+        if i <= 1:
+            valid = (c, r) not in visited
+            break
+        if valid:
+            break
+    
+    if valid == False:
+        raise Exception("no valid path for snake")
+
 with open("tests/00-example.txt") as topo_file:
     count = 0
     for line in topo_file:
@@ -61,9 +104,22 @@ for bound in range(1, max_snake + 1):
                 prev = dps[bound - 2]
                 
                 choices = [(prev[y % rows][x % cols], direction) for x, y, direction in possible_next]
-                optimal_path, direction = max(choices, key=lambda x: x[0])
+                paths, direction = zip(*sorted(choices, key=lambda x: x[0]))
                 
-                dp[row][col] = optimal_path + (0 if matrix[row][col] == '*' else int(matrix[row][col]))
+                # paths[0] is optimal, but we can't always take the optimal path because it might overlap itself
+                
+                # check for overlaps:
+                
+                assert len(paths) == len(direction)
+                
+                for possible_path_index in range(len(paths)):
+                    if not checkForOverlap(possible_path_index, dps_path, row, col, rows, cols, paths, wormholes):
+                        continue
+                    else:
+                        best_choice_index = paths[possible_path_index]
+                    
+                
+                dp[row][col] = paths[best_choice_index] + (0 if matrix[row][col] == '*' else int(matrix[row][col]))
                 dp_path[row][col] = direction
     dps.append(dp)
     dps_path.append(dp_path)
@@ -73,3 +129,5 @@ for r in dps[1]:
 
 for l in dps_path[1]:
     print(l)
+    
+    
